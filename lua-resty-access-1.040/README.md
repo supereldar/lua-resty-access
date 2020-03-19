2b8e45688d928fb8ef870ce5e3d3d56423aa4999
supereldar <eldar@beibutov.com>
2020-03-19 19:22:11 +0000

    Important Update

---
+++ lua-resty-access-1.040/README.md
@@ -0,0 +1,71 @@
+# lua-resty-access
+**lua-resty-access** - Web application access management module based on passwordless authentication for OpenResty.
+
+**Live Demo on** https://luarestyaccess.site
+
+## Installation
+```Shell
+$ opm get supereldar/lua-resty-access
+```
+## Requirements and Quick Start example
+Your nginx configuration should look like this 
+```nginx
+http {
+#REQUIREMENT: module require temporary storage, please setup luarestyaccess dictionary.
+  lua_shared_dict luarestyaccess 10m;
+  
+    server {
+    listen 80;
+    servername domain.local;
+    
+      location / {
+#REQUIREMENT: resolver and ca certificate directives are needed for external communication.
+        resolver 8.8.8.8;
+        lua_ssl_trusted_certificate /etc/ssl/certs/ca-certificates.crt;
+#REQUIREMENT: Call method Protect of resty.access object using access_by_lua* directive to activate access restriction.
+        access_by_lua_block {
+          local access = require'resty.access'
+          local site = access:new()
+          
+          #Add users one by one who can access this location. To pass authentication provide "username".
+          site:permitUser({username="john", email="john@snow.winter"})
+          
+          #You can also permit a single email.
+          site:permitEmail({email = "john@snow.winter"})
+          
+          #Or you can permit the whole domain. "*" - works as wildcard here.
+          site:permitEmail({email = "*@snow.winter"})
+          
+          #Launch module
+          site:protect()
+         }
+       proxy_pass http://app1.domain.local;
+     }
+   }
+}
+``` 
+## Optional configuration
+If you want to change access time and persistence or cookie name prefix you can use sessionConfig method.
+```shell
+site:sessionConfig({cookie_prefix = "luarestyaccess_", access_persistent = false , access_time = 3600})
+```
+If you want to keep users authenticated after configuration reload, specify static secret using access_secret key.
+
+```shell
+site:sessionConfig({access_secret = 623q4hR325t36VsCD3g567922IC0073T})
+```
+If you want grant access to the whole domain specify it's name using cookie_domain key.
+```shell
+site:sessionConfig({cookie_domain = "domain.local"})
+```
+If you want to process emails through your own smtp server you can use emailConfig method.
+```shell
+site:emailConfig({
+  mode = "smtp", 
+  host= "smtp.gmail.com", 
+  port = 587, 
+  tls = true,
+  username = "user@gmail.com",
+  password = "qwerty123"  
+})
+```
---
+++ lua-resty-access-1.040/lib/resty/access/email_actions.lua
@@ -0,0 +1,146 @@
+local M = {}
+local mail = require 'resty.mail'
+local http = require 'resty.http'
+local cjson = require 'cjson.safe'
+local str = require 'resty.string'
+
+local function validemail(str)
+  if str == nil then return nil end
+  if (type(str) ~= 'string') then
+    error("Expected string")
+    return nil
+  end
+  local lastAt = str:find("[^%@]+$")
+  local localPart = str:sub(1, (lastAt - 2)) -- Returns the substring before '@' symbol
+  local domainPart = str:sub(lastAt, #str) -- Returns the substring after '@' symbol
+  -- we werent able to split the email properly
+  if localPart == nil then
+    return nil, "Local name is invalid"
+  end
+
+  if domainPart == nil then
+    return nil, "Domain is invalid"
+  end
+  -- local part is maxed at 64 characters
+  if #localPart > 64 then
+    return nil, "Local name must be less than 64 characters"
+  end
+  -- domains are maxed at 253 characters
+  if #domainPart > 253 then
+    return nil, "Domain must be less than 253 characters"
+  end
+  -- somthing is wrong
+  if lastAt >= 65 then
+    return nil, "Invalid @ symbol usage"
+  end
+  -- quotes are only allowed at the beginning of a the local name
+  local quotes = localPart:find("[\"]")
+  if type(quotes) == 'number' and quotes > 1 then
+    return nil, "Invalid usage of quotes"
+  end
+  -- no @ symbols allowed outside quotes
+  if localPart:find("%@+") and quotes == nil then
+    return nil, "Invalid @ symbol usage in local part"
+  end
+  -- no dot found in domain name
+  if not domainPart:find("%.") then
+    return nil, "No TLD found in domain"
+  end
+  -- only 1 period in succession allowed
+  if domainPart:find("%.%.") then
+    return nil, "Too many periods in domain"
+  end
+  if localPart:find("%.%.") then
+    return nil, "Too many periods in local part"
+  end
+  -- just a general match
+  if not str:match('[%w]*[%p]*%@+[%w]*[%.]?[%w]*') then
+    return nil, "Email pattern test failed"
+  end
+  -- all our tests passed, so we are ok
+  return true
+end
+
+
+local function return_error(message,err)
+	if not err then err = " " end
+	ngx.log(ngx.ERR, message, err)
+	return false
+end
+
+local function htmlescape(string)
+        if not string then local string = false end
+        if string then
+                string = string:gsub("<", "&lt;")
+                string = string:gsub(">", "&gt;")
+                string = string:gsub("'", "&quot;")
+                string = string:gsub('"', "&quot;")
+                string = string:gsub("&", "&amp;")
+        end
+        return string
+end
+
+local function sendemail(to,otp,host,location,config)
+        local ok = true
+        local host = htmlescape(host)
+	local location = htmlescape(location)
+	location = location:gsub("#","")
+        if not validemail(to) then ok = return_error("email validation error: ",to) end
+        if not tonumber(otp) or string.len(otp) > 10 then ok = return_error("otp validation error: ",otp) end
+        
+        if ok and config["mode"] == "default" then
+                local body = {}
+                body['to'] = to
+                body['otp'] = otp
+                body['host'] = host
+                body['location'] = location
+                local httpc = http.new()
+                local res, err = httpc:request_uri("https://mail.service.luarestyaccess.site:443/", {
+                        method = "POST",
+                        path = "/",
+                        body = cjson.encode(body),
+                        ssl_verify= true,
+                        headers = {
+                                ["Host"] = "mail.service.luarestyaccess.site",
+                                ["luarestyaccesstoken"] = "623q4hR325t36VsCD3g567922IC0073T",
+                                ["Content-type"] = "application/json",
+                                ["Connection"] = "close"
+                        }
+                })
+                if err then ok = return_error("error send email: ", err) end
+                if res.status ~= 200 then ok = return_error("error send https request to mail.service.luarestyaccess.site. Reposnse code is ", res.status) end
+        end
+
+        if ok and config["mode"] == "smtp" then
+                local mailer, err = mail.new({
+                        host = config['host'],
+                        port = config['port'],
+                        starttls = config['starttls'],
+                        username = config ['username'],
+                        password = config ['password']
+                })
+                if err then ok = return_error("mail.new error: ", err) end
+                local url = "https://"..host..location
+                local uri = url.."#code="..otp
+
+--                local text = "Click the link below to finish your login to "..host.."\r\n"..url.." \r\n \r\nYou can also Copy"
+                local text = "Finish your login to "..host.."\r\nCopy and paste the code below into the login screen\r\n"..otp.." \r\nThis code will expire in 1 minute."
+--                local html = "Click the link below to finish your login to "..host.."<br><a href='"..uri.."'>"..uri.."</a><br><br>You can also"
+                local html = "Finish your login to "..host.."<br>Copy and paste the code below into the login screen<br><h2>"..otp.."</h2>This code will expire in 1 minute."
+
+                local success, err = mailer:send({
+                        from = "lua-resty-access <"..config ['username']..">",
+                        to = { to },
+                        subject = "Login code for "..host.." is "..otp,
+                        text = text,
+                        html = html
+                })
+                if err then ok = return_error("mailer:send error: ", err) end
+        end
+
+        return ok
+
+end
+M.check = validemail
+M.send = sendemail
+return M
